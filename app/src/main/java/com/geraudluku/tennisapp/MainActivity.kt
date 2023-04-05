fbf8177c9f6c5e0aa401afa85cc5b04cd0103f0c
GeraudLuku <geraud.luku@dme.systems>
2023-04-05 17:29:47 +0100

    UI Improvements

--- app/src/main/java/com/geraudluku/tennisapp/MainActivity.kt
+++ app/src/main/java/com/geraudluku/tennisapp/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -19,6 +21,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -27,7 +31,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.geraudluku.tennisapp.ui.theme.TennisAppTheme
-import com.geraudluku.tennisapp.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,6 +44,18 @@ class MainActivity : ComponentActivity() {
 @Preview(showSystemUi = true)
 @Composable
 fun DefaultPreview() {
+    val clubs = listOf(
+        Club(
+            "Women's Club",
+            colorResource(id = R.color.women),
+            painterResource(id = R.drawable.tennis_player)
+        ),
+        Club(
+            "Men's Club",
+            colorResource(id = R.color.men),
+            painterResource(id = R.drawable.men_tennis_player)
+        )
+    )
     TennisAppTheme {
         Scaffold(
             backgroundColor = Color.White,
@@ -50,36 +65,27 @@ fun DefaultPreview() {
                     contentPadding = PaddingValues(horizontal = 24.dp, vertical = 16.dp),
                     elevation = 0.dp
                 ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .clip(shape = RoundedCornerShape(10.dp))
-                                .background(Color.Black),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            IconButton(onClick = { /*TODO*/ }) {
-                                Icon(
-                                    imageVector = Icons.Default.Menu,
-                                    contentDescription = "Hamburger Menu",
-                                    modifier = Modifier.padding(2.dp),
-                                    tint = Color.White
-                                )
-                            }
-                        }
 
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                imageVector = Icons.Filled.Search,
-                                contentDescription = "Search Icon",
-                                tint = Color.Black,
-                                modifier = Modifier.size(32.dp)
-                            )
-                        }
+                    IconButton(modifier = Modifier
+                        .clip(shape = RoundedCornerShape(25))
+                        .background(Color.Black),
+                        onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "menu",
+                            tint = Color.White
+                        )
+                    }
+
+                    Spacer(Modifier.weight(1f))
+
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "Search Icon",
+                            tint = Color.Black,
+                            modifier = Modifier.size(32.dp)
+                        )
                     }
                 }
             },
@@ -87,23 +93,24 @@ fun DefaultPreview() {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
+                    .verticalScroll(rememberScrollState(), true)
                     .padding(start = 24.dp)
             ) {
+
                 Text(
                     text = stringResource(id = R.string.clubs),
-                    color = colorResource(id = R.color.black),
+                    color = Color.Black,
                     fontWeight = FontWeight.Bold,
-                    fontSize = MaterialTheme.typography.h3.fontSize
+                    fontSize = MaterialTheme.typography.h3.fontSize,
+                    modifier = Modifier.padding(bottom = 20.dp)
                 )
-
-                Spacer(modifier = Modifier.size(10.dp))
 
                 LazyRow(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.spacedBy(10.dp)
                 ) {
-                    items(2) {
-                        ClubItem()
+                    items(clubs) { club ->
+                        ClubItem(club)
                     }
                 }
 
@@ -112,29 +119,27 @@ fun DefaultPreview() {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(end = 16.dp),
+                        .padding(end = 16.dp, bottom = 10.dp),
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Text(
                         text = "Train",
                         color = Color.Black,
-                        fontSize = MaterialTheme.typography.h4.fontSize,
+                        fontSize = MaterialTheme.typography.h5.fontSize,
                         fontWeight = FontWeight.Bold
                     )
 
                     Text(
                         text = "Alles",
                         color = colorResource(id = R.color.green),
-                        fontSize = 19.sp,
+                        fontSize = 18.sp,
                         fontWeight = FontWeight.Bold
                     )
                 }
 
-                Spacer(modifier = Modifier.size(10.dp))
-
-                LazyColumn() {
-                    items(2) {
+                Column {
+                    for (i in 1..2) {
                         TrainItem()
                     }
                 }
@@ -146,22 +151,37 @@ fun DefaultPreview() {
 
 @Preview(showBackground = true)
 @Composable
-fun ClubItem(title: Int = R.string.women_club, image: Int = R.drawable.tennis_player) {
+fun ClubItem(
+    club: Club = Club(
+        "Women's Club",
+        colorResource(id = R.color.men),
+        painterResource(id = R.drawable.men_tennis_player)
+    )
+) {
+
     Box(
         modifier = Modifier
             .clip(shape = RoundedCornerShape(30.dp))
-            .background(colorResource(id = R.color.row1)),
+            .background(club.color),
         contentAlignment = Alignment.Center
     ) {
-        Row(modifier = Modifier.padding(vertical = 50.dp, horizontal = 20.dp)) {
-            Column {
+
+        Row(
+            modifier = Modifier
+                .padding(start = 20.dp)
+                .padding(top = 25.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Column(Modifier.width(150.dp)) {
                 Text(
                     modifier = Modifier.padding(bottom = 10.dp),
-                    text = stringResource(id = title),
+                    text = club.title,
                     color = Color.Black,
                     fontSize = MaterialTheme.typography.h4.fontSize,
                     fontWeight = FontWeight.Bold
                 )
+
                 Button(
                     onClick = { /*TODO*/ },
                     colors = ButtonDefaults.textButtonColors(
@@ -175,20 +195,21 @@ fun ClubItem(title: Int = R.string.women_club, image: Int = R.drawable.tennis_pl
                     )
                 }
 
-                Spacer(modifier = Modifier.height(50.dp))
+                Spacer(modifier = Modifier.height(180.dp))
 
                 Text(
                     text = stringResource(id = R.string.events),
                     color = Color.Black,
-                    fontSize = 22.sp,
+                    fontSize = MaterialTheme.typography.h5.fontSize,
+                    modifier = Modifier.padding(bottom = 50.dp)
                 )
             }
 
             //Tennis Player Image
             Image(
-                painter = painterResource(id = image),
+                painter = club.image,
                 contentDescription = "A poster image of a tennis player",
-//                contentScale = ContentScale.FillBounds
+                contentScale = ContentScale.FillBounds
             )
         }
     }
@@ -250,5 +271,13 @@ fun TrainItem() {
                 modifier = Modifier.padding(7.dp),
             )
         }
+
     }
 }
+
+
+data class Club(
+    var title: String,
+    var color: Color,
+    var image: Painter
+)
